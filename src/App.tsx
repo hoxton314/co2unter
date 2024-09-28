d3b278105e5d3b4a0e540de9579372ebd9002d3d
Igor Kaliciński <igorkalicinski@gmail.com>
2024-09-28 15:28:07 +0200

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react'
+import './App.css'
+import { AppContainer } from './styles/App.styles'
+import { GlobalStyles } from './styles/Global.styles'
+import { Helmet } from './components/Helmet'
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer className="App">
+      <Helmet title="CO2UNTER" description="CO2UNTER" themeColor="#ffffff" />
+      <GlobalStyles />
+    </AppContainer>
+  )
 }
 
-export default App;
+export default App
---
+++ src/store/AppState.store.tsx
@@ -0,0 +1,26 @@
+import { action, makeAutoObservable } from 'mobx'
+import { RootStore } from './Root.store'
+
+export type Theme = 'light' | 'dark'
+
+const DEV_MODE = process.env.DEV_MODE === 'true'
+
+export class AppStateStore {
+  DEV_MODE = DEV_MODE || false
+  rootStore
+  theme = 'dark' as Theme
+
+  constructor(rootStore: RootStore) {
+    makeAutoObservable(this)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    this.rootStore = rootStore
+  }
+
+  @action.bound setTheme(theme: Theme) {
+    this.theme = theme
+  }
+
+  @action.bound toggleTheme() {
+    this.theme = this.theme === 'light' ? 'dark' : 'light'
+  }
+}

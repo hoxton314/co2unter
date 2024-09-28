6dec3b97f0630fde3d3c803e99c92e4ee766c4ac
Igor Kaliciński <igorkalicinski@gmail.com>
2024-09-28 14:59:01 +0200

    fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ import { merge } from 'webpack-merge'
 import common from './webpack.common.js'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
-import { GenerateSW } from 'workbox-webpack-plugin'
+import { GenerateSW, InjectManifest } from 'workbox-webpack-plugin'
 import Dotenv from 'dotenv-webpack'
 import pkg from 'webpack'
 
@@ -30,6 +30,10 @@ export default merge(common, {
       swDest: 'service-worker.js', // Output name for the service worker file
       clientsClaim: true, // Allows the service worker to take control of all clients once it's activated
       skipWaiting: true, // Skip waiting period for new SW to activate
+    }),
+    new InjectManifest({
+      swSrc: './src/service-worker.ts', // Path to your custom service worker file
+      swDest: 'service-worker.js', // Output service worker file name
     }),
   ],
 })

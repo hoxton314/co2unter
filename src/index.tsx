2666d3cfb8fc143235624d1e685625936cb896be
Igor Kaliciński <igorkalicinski@gmail.com>
2024-09-28 12:38:51 +0200

    PWA serviceworker added

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import { App } from '@components/App'
+// Other imports
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
 root.render(<App />)
+
+// Register the service worker
+serviceWorkerRegistration.register()

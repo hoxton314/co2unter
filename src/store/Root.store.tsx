1edae0811e41c7e873996cc550a1ab2148e5abb0
dziwuj <sobeckizbyszko@gmail.com>
2024-09-28 22:56:33 +0200

    Fixes after merge with main

--- src/store/Root.store.tsx
+++ src/store/Root.store.tsx
@@ -1,13 +1,16 @@
 import { AppStateStore } from './AppState.store'
-import { FormStateStore } from './Form.store'
+import { FormStateStore } from './FormState.store'
+import { TranslationsStore } from './TranslationsState.store'
 
 export class RootStore {
   AppState: AppStateStore
   FormState: FormStateStore
+  TranslationsState: TranslationsStore
 
   constructor() {
     this.AppState = new AppStateStore(this)
     this.FormState = new FormStateStore(this)
+    this.TranslationsState = new TranslationsStore(this)
   }
 }
 

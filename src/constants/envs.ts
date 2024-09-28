5e5c3a20f9ccb4d56ac9b42726e485cc232322b3
Igor Kaliciński <igorkalicinski@gmail.com>
2024-09-28 18:32:43 +0200

    fix

--- src/constants/envs.ts
+++ src/constants/envs.ts
@@ -1,3 +1,3 @@
-export const API_URL = process.env.REACT_APP_API_URL || window.location.origin
+export const API_URL = process.env.REACT_APP_API_URL || 'https://co2unter-api.hoxton.dev/'
 
 export const DEV_MODE = process.env.REACT_APP_DEV_MODE === 'true' || false
